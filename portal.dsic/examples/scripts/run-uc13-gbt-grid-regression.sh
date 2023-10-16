c6a514353b92508dfc936e3fdd32685639d82568
Jon Ander Gómez <jon@dsic.upv.es>
2023-10-16 22:11:11 +0200

    Added new script for running grid using GBTRegressor

---
+++ portal.dsic/examples/scripts/run-uc13-gbt-grid-regression.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# patient 11 must be removed
+for p in 03 07 08 09 10 11 12 13 15 16 18 19 22 24
+do
+    patient="chb${p}"
+    num_trees="20:30:50:100:200"
+    #num_trees="100"
+    max_depth="3:5:7:9"
+    #max_depth="5"
+    #format="pca136"
+    format="21x14"
+
+    scripts/run-python.sh python/gbt_uc13_regression.py ${patient} \
+                                                            --numTrees ${num_trees} \
+                                                            --maxDepth ${max_depth} \
+                                                            --dataFormat ${format} \
+                                                            --verbose 0
+done
